5ddcff8f29d9862f1ddeb04fa33a8a5f0208fc08
August Dolan <august.dolan1@gmail.com>
2023-05-25 09:37:33 -0400

    First pass at getProduct

---
+++ src/getProductOfAllIntsExceptAtIndex.js
@@ -0,0 +1,101 @@
+// Must be done without division
+
+function getProductsOfAllIntsExceptAtIndex(intArray) {
+  if (intArray.length < 2) {
+    throw new Error();
+  }
+  const productArray = [];
+  let beforeIndexProduct = 1;
+  let afterIndexProduct = 1;
+  let beforeIndexProductArr = [1]
+  let afterIndexProductArr = [1]
+
+  for (let i = 0; i < intArray.length - 1; i++) {
+    beforeIndexProductArr.push(beforeIndexProduct *= intArray[i]);
+  }
+
+  for (let i = intArray.length - 1; i > 0; i--) {
+    afterIndexProductArr.push(afterIndexProduct *= intArray[i]);
+  }
+  afterIndexProductArr.reverse();
+
+  for (let i = 0; i < beforeIndexProductArr.length; i++) {
+    productArray.push(beforeIndexProductArr[i] *= afterIndexProductArr[i]);
+  }
+
+  return productArray;
+}
+
+// brute force method would be o(n^2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'short array';
+let actual = getProductsOfAllIntsExceptAtIndex([1, 2, 3]);
+let expected = [6, 3, 2];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'longer array',
+actual = getProductsOfAllIntsExceptAtIndex([8, 2, 4, 3, 1, 5]);
+expected = [120, 480, 240, 320, 960, 192];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'array has one zero',
+actual = getProductsOfAllIntsExceptAtIndex([6, 2, 0, 3]);
+expected = [0, 0, 36, 0];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'array has two zeros';
+actual = getProductsOfAllIntsExceptAtIndex([4, 0, 9, 1, 0]);
+expected = [0, 0, 0, 0, 0];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'one negative number';
+actual = getProductsOfAllIntsExceptAtIndex([-3, 8, 4]);
+expected = [32, -12, -24];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'all negative numbers';
+actual = getProductsOfAllIntsExceptAtIndex([-7, -1, -4, -2]);
+expected = [-8, -56, -14, -28];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'error with empty array';
+const emptyArray = () => (getProductsOfAllIntsExceptAtIndex([]));
+assertThrowsError(emptyArray, desc);
+
+desc = 'error with one number';
+const oneNumber = () => (getProductsOfAllIntsExceptAtIndex([1]));
+assertThrowsError(oneNumber, desc);
+
+function assertArrayEquals(a, b, desc) {
+  const arrayA = JSON.stringify(a);
+  const arrayB = JSON.stringify(b);
+  if (arrayA !== arrayB) {
+    console.log(`${desc} ... FAIL: ${arrayA} != ${arrayB}`)
+  } else {
+    console.log(`${desc} ... PASS`);
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
